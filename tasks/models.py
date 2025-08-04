bb3f45aa3b07a6beb5da678a8c8a6c999e61ece4
harishragul <harishragul@protonmail.com>
2025-08-04 09:13:23 +0000

    aug-04-update

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    task = models.CharField(max_length=128, null=False, blank=False)
+    assigned_to = models.CharField(max_length=128, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.id}: {self.task}"
--- tasks/views.py
+++ tasks/views.py
@@ -1,23 +1,26 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from tasks.models import Task
 
 # Create your views here.
 
-tasks = []
-
-def main(request):
-    task_data = {}
-    
+def main(request):    
     if request.POST:
         task = request.POST.get("task")
         assigned_to = request.POST.get("assigned_to")
 
-        task_data["task"] = task
-        task_data["assigned_to"] = assigned_to
+        # to store the data into DB
+        task_instance = Task(
+            task=task,
+            assigned_to=assigned_to
+        )
+        task_instance.save()
 
-        tasks.append(task_data)
+        return redirect("task_main")
+
+    # to read data from DB (all data from a table)
+    tasks = Task.objects.all()
 
     context = {
         "tasks": tasks
     }
-
     return render(request, "main.html", context)
