d4b7bddf8bacba49abef20de4a0331e66733f00f
harishragul <harishragul@protonmail.com>
2025-08-12 09:03:05 +0000

    aug-12 Add Status and Task models with fields for task management and tracking

--- tasks/models.py
+++ tasks/models.py
@@ -1,9 +1,34 @@
 from django.db import models
+from teams.models import Member, Organization
+from django.utils import timezone
 
 # Create your models here.
+
+class Status(models.Model):
+    name = models.CharField(max_length=128, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Task(models.Model):
     task = models.CharField(max_length=128, null=False, blank=False)
-    assigned_to = models.CharField(max_length=128, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    priority = models.IntegerField(default=None, null=True)
+    assigned_to = models.ForeignKey(Member, related_name="assigned_to", default=None, null=True, on_delete=models.SET_NULL)
+    assigned_by = models.ForeignKey(Member, related_name="assigned_by", default=None, null=True, on_delete=models.SET_NULL)
+    status = models.ForeignKey(Status, default=None, null=True, on_delete=models.SET_NULL)
+    total_effort = models.IntegerField(default=None, null=True)
+    acutal_effort = models.IntegerField(default=None, null=True)
+    remaining_effort = models.IntegerField(default=None, null=True)
+    due = models.DateTimeField(default=None)
+
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.id}: {self.task}"
--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from uuid import uuid5
+from django.utils import timezone
 
 # Create your models here.
+class Organization(models.Model):
+    name = models.CharField(max_length=256, null=False, blank=False)
+    email = models.EmailField()
+    website = models.CharField(max_length=256, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    city = models.CharField(max_length=256, null=True, blank=True)
+    zipcode = models.CharField(max_length=10, null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id}: {self.name}"
+
+class Role(models.Model):
+    name = models.CharField(max_length=256, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Member(models.Model):
+    member_id = models.UUIDField(uuid5, unique=True, null=False, blank=False, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, null=True, on_delete=models.SET_NULL)
+    is_active = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.first_name}: {self.role}"
