c2ec91e7d1b582bd0df755bcc6e90e4655ef43ea
harishragul <harishragul@protonmail.com>
2025-08-19 08:49:54 +0000

    aug-19 Register Organization model in Django admin with custom admin options

--- teams/admin.py
+++ teams/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from teams.models import Organization
 # Register your models here.
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'website', 'city']
+    readonly_fields = ['created_at', 'update_at']
+    list_filter = ['city']
+    search_fields = ['id', 'name']
+    sortable_by = ['id', 'name']
+
+#admin.site.register(Organization, OrganizationAdmin)
